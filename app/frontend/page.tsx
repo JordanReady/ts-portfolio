339be93efd34c4eecaab17aade239059a2db4dc1
JordanReady <jready112@gmail.com>
2023-12-19 03:20:04 -0600

    add: Project Card / Frontend cards

--- app/frontend/page.tsx
+++ app/frontend/page.tsx
@@ -1,8 +1,71 @@
 import React from "react";
 import styles from "./FrontEnd.module.css";
+import ProjectCard from "@/components/ProjectCard";
+import MoreButton from "@/public/MoreBtn.png";
+import News from "@/public/LogoDesktopNews1.png";
+import Booking from "@/public/LogoDesktopBooking1.png";
+import Youtube from "@/public/LogoDesktopYoutube1.png";
+import BookingPage from "@/public/PageBooking1.png";
+import BookingPage2 from "@/public/PageBooking2.png";
+import BookingPage3 from "@/public/PageBooking3.png";
+import BookingPage4 from "@/public/PageBooking4.png";
+import NewsPage from "@/public/PageNews1.png";
+import NewsPage2 from "@/public/PageNews2.png";
+import NewsPage3 from "@/public/PageNews3.png";
+import NewsPage4 from "@/public/PageNews4.png";
+import YoutubePage from "@/public/PageYoutube1.png";
+import YoutubePage2 from "@/public/PageYoutube2.png";
+import YoutubePage3 from "@/public/PageYoutube3.png";
+import YoutubePage4 from "@/public/PageYoutube4.png";
 
 function FrontEnd() {
-  return <div>FrontEnd</div>;
+  const type = "frontend";
+  const projectData = {
+    type: type,
+    number: 1,
+    name: "AirBnb News",
+    description: "Responsive AirBnb News website clone",
+    technologies: "React, HTML, CSS, Bootstrap",
+    background: News,
+    logos: [NewsPage, NewsPage2, NewsPage3, NewsPage4],
+    moreButton: MoreButton,
+  };
+  const projectData2 = {
+    type: type,
+    number: 2,
+    name: "YouTube Clone",
+    description: "Responsive YouTube website clone",
+    technologies: "React, CSS, Material UI, Rapid API",
+    background: Youtube,
+    logos: [YoutubePage, YoutubePage2, YoutubePage3, YoutubePage4],
+    moreButton: MoreButton,
+  };
+  const projectData3 = {
+    type: type,
+    number: 3,
+    name: "AirBnb Booking",
+    description: "Responsive AirBnb Booking website clone",
+    technologies: "HTML, CSS, Bootstrap",
+    background: Booking,
+    logos: [BookingPage, BookingPage2, BookingPage3, BookingPage4],
+    moreButton: MoreButton,
+  };
+
+  return (
+    <div className="container">
+      <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
+        <div className={styles.card}>
+          <ProjectCard {...projectData} />
+        </div>
+        <div className={styles.card}>
+          <ProjectCard {...projectData2} />
+        </div>
+        <div className={styles.card}>
+          <ProjectCard {...projectData3} />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default FrontEnd;
---
+++ components/ProjectCard.tsx
@@ -0,0 +1,82 @@
+import React from "react";
+import styles from "./ProjectCard.module.css";
+import Image, { StaticImageData } from "next/image";
+import { Button } from "./ui/button";
+import Link from "next/link";
+
+interface ProjectCardProps {
+  type: string;
+  number: number;
+  name: string;
+  description: string;
+  technologies: string;
+  background: StaticImageData;
+  logos: StaticImageData[]; // Change to array
+  moreButton: StaticImageData;
+}
+
+function ProjectCard({
+  type,
+  number,
+  name,
+  description,
+  technologies,
+  background,
+  logos,
+  moreButton,
+}: ProjectCardProps) {
+  const backgroundStyle = {
+    backgroundImage: `url(${background.src})`,
+    backgroundSize: "contain",
+    backgroundPosition: "center",
+    backgroundRepeat: "no-repeat",
+  };
+  const techArray = technologies.split(",");
+
+  return (
+    <Link href={`/${type}/${number}`}>
+      <div
+        className={`${styles.projectCard} shadow-purple-300 dark:shadow-purple-700  shadow-sm border border-b-1 gap-2`}
+        style={backgroundStyle}
+      >
+        <div className={styles.content}>
+          {/* <h2 className={`${styles.number}`}>0{number}</h2>
+          <h2 className={`${styles.numberMain}`}>0{number}</h2> */}
+          <div className={`${styles.innerBox}`}>
+            <h3 className={`${styles.name}`}>{name}</h3>
+            <p
+              className={` text-slate-700 dark:text-gray-100 ${styles.description}`}
+            >
+              {description}
+            </p>
+          </div>
+          <div className={`${styles.technologies}`}>
+            <strong>Technologies:</strong>
+            {techArray.map((tech, index) => (
+              <p key={index} className="text-gray-700 dark:text-gray-100">
+                {tech}
+              </p>
+            ))}
+          </div>
+          <div className={styles.pages}>
+            {logos.map((logo, index) => (
+              <Image
+                key={index}
+                className={`${styles.img}`}
+                src={logo}
+                alt={`Logo ${index + 1}`}
+              ></Image>
+            ))}
+          </div>
+          {/* <Image
+            className={`${styles.btn}`}
+            src={moreButton}
+            alt="More Button"
+          ></Image> */}
+        </div>
+      </div>
+    </Link>
+  );
+}
+
+export default ProjectCard;
