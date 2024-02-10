4d9b8050c6dff4876289c15576cfcee52207bb36
JordanReady <jready112@gmail.com>
2024-02-10 13:14:13 -0600

    fix: small ui fixes

--- app/contact/thankyou/page.tsx
+++ app/contact/thankyou/page.tsx
@@ -13,62 +13,44 @@ function ThankYou() {
     >
       {/* Left Column */}
       <div className="md:col-span-1 flex justify-center">
-        <Reveal direction="top" delayTime={0} color="#9333ea">
+        <div
+          className={`relative p-0.5 mb-2  overflow-hidden text-sm font-medium rounded-lg ${styles.contactContainer} bg-gradient-to-br from-purple-600 to-blue-500 `}
+        >
           <div
-            className={`relative p-0.5 mb-2  overflow-hidden text-sm font-medium rounded-lg ${styles.contactContainer} bg-gradient-to-br from-purple-600 to-blue-500 `}
+            className={`max-w-md mx-auto relative px-5 py-2.5 ${styles.contactCard} bg-white dark:bg-slate-900 rounded-md group-hover:bg-opacity-0`}
           >
+            <h2 className="text-transparent bg-clip-text bg-gradient-to-br from-purple-600 to-blue-500 text-4xl mb-2 font-semibold">
+              Jordan Ready
+            </h2>
             <div
-              className={`max-w-md mx-auto relative px-5 py-2.5 ${styles.contactCard} bg-white dark:bg-slate-900 rounded-md group-hover:bg-opacity-0`}
+              className={`${styles.logo} relative p-0.5 mb-2  overflow-hidden text-sm font-medium rounded-lg bg-gradient-to-br from-purple-600 to-blue-500`}
             >
-              <Reveal direction="top" delayTime={0.2} color="#9333ea">
-                <h2 className="text-transparent bg-clip-text bg-gradient-to-br from-purple-600 to-blue-500 text-4xl mb-2 font-semibold">
-                  Jordan Ready
-                </h2>
-              </Reveal>
-              <Reveal direction="top" delayTime={0.3} color="#9333ea">
-                <div
-                  className={`${styles.logo} relative p-0.5 mb-2  overflow-hidden text-sm font-medium rounded-lg bg-gradient-to-br from-purple-600 to-blue-500`}
-                >
-                  <Image
-                    src={Logo}
-                    alt="Logo"
-                    width={130}
-                    height={130}
-                    className={`${styles.logoImg}  bg-white dark:bg-slate-900 `}
-                    loading="lazy"
-                    placeholder="blur"
-                    sizes="fill"
-                  />
-                </div>
-              </Reveal>
-              <div className={styles.contactInfo}>
-                <Reveal direction="top" delayTime={0.4} color="#9333ea">
-                  <p className="text-transparent bg-clip-text bg-gradient-to-br from-purple-600 to-blue-500 text-3xl mb-2 font-semibold">
-                    Personal Info
-                  </p>
-                </Reveal>
-                <Reveal direction="top" delayTime={0.45} color="grey">
-                  <p className=" mb-2">Software Developer</p>
-                </Reveal>
-                <Reveal direction="top" delayTime={0.5} color="grey">
-                  <p className=" mb-2">Location: Dubuque, IA</p>
-                </Reveal>
-                <Reveal direction="top" delayTime={0.55} color="grey">
-                  <p className=" mb-2">Phone: 563-593-3110</p>
-                </Reveal>
-                <Reveal direction="top" delayTime={0.6} color="grey">
-                  <p className=" mb-4">Email: jready112@gmail.com</p>
-                </Reveal>
-                <Reveal direction="top" delayTime={0.7} color="#9333ea">
-                  <CustomButton
-                    link="https://www.linkedin.com/in/your-linkedin"
-                    text="LinkedIn"
-                  />
-                </Reveal>
-              </div>
+              <Image
+                src={Logo}
+                alt="Logo"
+                width={130}
+                height={130}
+                className={`${styles.logoImg}  bg-white dark:bg-slate-900 `}
+                loading="lazy"
+                placeholder="blur"
+                sizes="fill"
+              />
+            </div>
+            <div className={styles.contactInfo}>
+              <p className="text-transparent bg-clip-text bg-gradient-to-br from-purple-600 to-blue-500 text-3xl mb-2 font-semibold">
+                Personal Info
+              </p>
+              <p className=" mb-2">Software Developer</p>
+              <p className=" mb-2">Location: Dubuque, IA</p>
+              <p className=" mb-2">Phone: 563-593-3110</p>
+              <p className=" mb-4">Email: jready112@gmail.com</p>
+              <CustomButton
+                link="https://www.linkedin.com/in/your-linkedin"
+                text="LinkedIn"
+              />
             </div>
           </div>
-        </Reveal>
+        </div>
       </div>
 
       {/* Right Column */}
