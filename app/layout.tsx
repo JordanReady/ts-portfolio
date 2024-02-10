243e1f974db07631210d47cf8910dfea5ff1d68c
JordanReady <jready112@gmail.com>
2024-02-10 12:45:35 -0600

    fix: font

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,7 +32,12 @@ export default async function RootLayout({
     <SessionProvider session={session}>
       <html lang="en">
         <head>
-          <link rel="icon" href="/favicon.ico" />
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Instrument+Serif&family=Montserrat:ital,wght@0,100..900;1,100..900&family=Protest+Strike&display=swap"
+            rel="stylesheet"
+          />
         </head>
         <body className={inter.className}>
           <FirebaseAuthProvider>
