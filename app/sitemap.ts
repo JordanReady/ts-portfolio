83c4c0261b65fed0783d934c12a73b2c0b5c5ba7
JordanReady <jready112@gmail.com>
2024-02-20 11:00:43 -0600

    add: sitemap / robots

---
+++ app/sitemap.ts
@@ -0,0 +1,75 @@
+import { MetadataRoute } from "next";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: "https://www.jordanready.com/",
+    },
+    {
+      url: "https://www.jordanready.com/about",
+    },
+    {
+      url: "https://www.jordanready.com/contact",
+    },
+    {
+      url: "https://www.jordanready.com/reviews",
+    },
+    {
+      url: "https://www.jordanready.com/profile",
+    },
+    {
+      url: "https://www.jordanready.com/projects",
+    },
+    {
+      url: "https://www.jordanready.com/production-sites",
+    },
+    {
+      url: "https://www.jordanready.com/production-sites/1",
+    },
+    {
+      url: "https://www.jordanready.com/production-sites/2",
+    },
+    {
+      url: "https://www.jordanready.com/fullstack",
+    },
+    {
+      url: "https://www.jordanready.com/fullstack/1",
+    },
+    {
+      url: "https://www.jordanready.com/fullstack/2",
+    },
+    {
+      url: "https://www.jordanready.com/fullstack/3",
+    },
+    {
+      url: "https://www.jordanready.com/frontend",
+    },
+    {
+      url: "https://www.jordanready.com/frontend/1",
+    },
+    {
+      url: "https://www.jordanready.com/frontend/2",
+    },
+    {
+      url: "https://www.jordanready.com/frontend/3",
+    },
+    {
+      url: "https://www.jordanready.com/frontend/4",
+    },
+    {
+      url: "https://www.jordanready.com/frontend/5",
+    },
+    {
+      url: "https://www.jordanready.com/services_",
+    },
+    {
+      url: "https://www.jordanready.com/services_/contract",
+    },
+    {
+      url: "https://www.jordanready.com/services_/designing",
+    },
+    {
+      url: "https://www.jordanready.com/services_/developing",
+    },
+  ];
+}
